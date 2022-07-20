4749dfa60f478d1b41bc9c1460aafa861d568417
afutofu <theafuza@gmail.com>
2022-07-20 14:16:56 +0700

    Made header and applied basic styling

---
+++ src/sections/home/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div id="header">
+        <div className="container">
+        <i>I</i>
+      <h1 id="title">DannyBoiTattoos</h1>
+      <h2>Tel: +1 (425) 245-9998</h2>
+        </div>
+      
+    </div>
+  );
+};
+
+export default Header;
