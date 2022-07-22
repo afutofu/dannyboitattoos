bf72a16d0e989c5ad02ab15d2bc3b6f725138a5b
afutofu <theafuza@gmail.com>
2022-07-22 14:39:31 +0700

    Created initial Gallery section template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles/App.css";
 
 import Home from "./sections/home/Home";
+import Gallery from "./sections/gallery/Gallery";
 
 function App() {
   return (
     <div id="app">
       <Home />
+      <Gallery />
     </div>
   );
 }
---
+++ src/sections/gallery/Gallery.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const Gallery = () => {
+  const [pics, setPics] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+  return (
+    <section id="gallery">
+      <div className="container">
+        <div className="info">
+          <h2>Gallery</h2>
+          <h3>Latest Works</h3>
+          <p>
+            More works available on{" "}
+            <a
+              href="https://www.instagram.com/dannyboimendez88/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="image-container">
+        <div className="image-container-1">
+          {pics.map((el) => {
+            return <div className="image">{el}</div>;
+          })}
+        </div>
+        <div className="image-container-2">
+          {pics.map((el) => {
+            return <div className="image">{el}</div>;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
