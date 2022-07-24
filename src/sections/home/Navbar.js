d30291793bfceace6fafb2d3374baab0000a6723
afutofu <theafuza@gmail.com>
2022-07-24 18:37:06 +0700

    Navbar now functional. Added hover animations to links and navitems

--- src/sections/home/Navbar.js
+++ src/sections/home/Navbar.js
@@ -1,11 +1,21 @@
+import { Link } from "react-scroll";
+
 const Navbar = () => {
   return (
     <div id="navbar">
       <div className="container">
-        <h3>HOME</h3>
-        <h3>GALLERY</h3>
-        <h3>BOOKING</h3>
-        <h3>CONTACT</h3>
+        <Link to="home" smooth={true} duration={1000}>
+          <h3>HOME</h3>
+        </Link>
+        <Link to="gallery" smooth={true} duration={1000}>
+          <h3>GALLERY</h3>
+        </Link>
+        <Link to="booking" smooth={true} duration={1000}>
+          <h3>BOOKING</h3>
+        </Link>
+        <Link to="contact" smooth={true} duration={1000}>
+          <h3>CONTACT</h3>
+        </Link>
       </div>
     </div>
   );
